d858e0c0ee4a60c4dc9be7ca26a3c08aa79037d8
Surya <surya.greatest@gmail.com>
2021-03-11 17:33:58 +0900

    Day6

--- Day6/6.py
+++ Day6/6.py
@@ -1,23 +1,28 @@
 import functools
 import string
 
-f = open("./TestData.txt", "r")
+f = open("./InputData.txt", "r")
 raw_data = f.read()
 
 data = raw_data.split('\n\n')
-processed_data = [entry.split('\n') for entry in data]
+list_data = [entry.split('\n') for entry in data]
 
-flat_data = [functools.reduce(lambda a, b: a + b, entry) for entry in processed_data]
-set_data = [map(lambda a: [i for i in a], processed_data)]
+flat_data = [functools.reduce(lambda a, b: a + b, entry) for entry in list_data]
+set_data = [[set(list(i)) for i in entry] for entry in list_data]
 
 def get_union(input_list):
-    total_length = 0
+    total_count = 0
     for element in input_list:
-        total_length += len(set(element))
-    return total_length
+        union_count = len(set(element)) 
+        total_count += union_count
+    return total_count
 
 def get_intersection(input_list):
-    total_length = 0
-    return None
+    total_count = 0
+    for entry in set_data:
+        intersection_count = len(entry[0].intersection(*entry))
+        total_count += intersection_count
+    return total_count
 
-print("Total questions answered : {0}".format(get_union(flat_data)))
+print("Total Union Count : {0:>12} \nTotal Intersection Count : {1:>5}".format(
+    get_union(flat_data), get_intersection(set_data)))
