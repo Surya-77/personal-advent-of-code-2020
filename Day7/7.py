39311e5fe05b559efa1054798a92516ae94c7d33
Surya <surya.greatest@gmail.com>
2021-03-15 16:11:02 +0900

    Day7 incomplete

--- Day7/7.py
+++ Day7/7.py
@@ -1,6 +1,7 @@
 import re
 import itertools
 import pprint
+from treelib import Tree, Node
 
 f = open("./TestData.txt", "r")
 raw_data = f.read()
@@ -16,44 +17,11 @@ for entry in list_data:
 
 pd_flag = [[False for column in row] for row in pd]
 
-target_bag = 'shiny gold'
-other_target_bag = 'shiny gold'
-count = 0
-i = 0
-
-
-
-# list_of_bag_colors = []
-# list_of_sub_bag_colors = []
-# list_of_sub_bag_contents = []
-# for entry in pd:
-#     bag_color = entry[0]
-#     sub_bag_colors = list(map(lambda a:' '.join(a.split(' ')[1:]), entry[1:]))
-#     sub_bag_count = list(map(lambda a:a.split(' ')[0], entry[1:]))
-#     sub_bag_contents = list(zip(sub_bag_colors, sub_bag_count))
-#     list_of_bag_colors.append(bag_color)
-#     list_of_sub_bag_colors.append(sub_bag_colors)
-#     list_of_sub_bag_contents.append(sub_bag_contents)
-
-# dict_of_entries = {}
-# for entry in zip(list_of_bag_colors, list_of_sub_bag_contents):
-#     if len(entry[1]) > 0:
-#         sub_entry_dict = {}
-#         for sub_entry in entry[1]:
-#             sub_entry_dict[sub_entry[0]] = sub_entry[1]
-#         dict_of_entries[entry[0]] = sub_entry_dict
-
-# pprint.pprint(dict_of_entries)
-
-# for outer_entry in dict_of_entries:
-#     for inner_entry in dict_of_entries:
-#         if outer_entry in dict(dict_of_entries[inner_entry].keys()):a
-#             dict_of_entries[outer_entry]
-
-
-
-
-
-
-
-
+tree_list = []
+for i in range(len(pd)):
+    tree_list.append(Tree())
+    tree_list[i].create_node(pd[i][0], pd[i][0])
+    for j in range(1, len(pd[i])):
+        bag_name = ' '.join(pd[i][j].split(' ')[1:])
+        bag_count = int(pd[i][j].split(' ')[0])
+        
