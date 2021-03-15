4c3d80e625ea1e8233c4895d169e76a89f1259c9
Surya <surya.greatest@gmail.com>
2021-03-16 00:09:55 +0900

    Day 7 complete

--- Day7/7.py
+++ Day7/7.py
@@ -1,7 +1,7 @@
 import re
 import itertools
 import pprint
-
+from copy import deepcopy
 
 f = open("./InputData.txt", "r")
 raw_data = f.read()
@@ -51,15 +51,79 @@ for iter_i in range(6):
             for sub_bag in sub_bags:
                 if sub_bag not in bags_to_check_2:
                     bags_to_check_2.append(sub_bag)
-            bag_rec_list.append([i[0], zip(sub_bags, sub_bags_count)])
-            # bags_to_check_2_count.append(sub_bags_count)
+            bag_rec_list.append([i[0], sub_bags, sub_bags_count, 1])
 
 
-for count in len(bag_rec_list):
-    pprint.pprint("Main Bag : {0}\n Sub Bags : {1}\n".format(count[0], list(count[1])))
+bag_rec_list_names = [bag[0] for bag in bag_rec_list]
+bag_rec_unique = []
 
-# pprint.pprint(bags_to_check_2)
-# pprint.pprint(list(zip(bags_to_check_2, bags_to_check_2_count)))
+for bag in bag_rec_list:
+    if bag[0] not in [i[0] for i in bag_rec_unique]:
+        bag_rec_unique.append(bag)
+
+# bag_rec_unique_original = deepcopy(bag_rec_unique)
+# bag_t1 = None
+# for iter_i in range(5):
+#     for bag_1 in list(bag_rec_unique):
+#         if len(bag_1[1]) == iter_i:
+#             bag_t1 = bag_1[0]
+#             removal_index_1 = [i[0] for i in bag_rec_unique].index(bag_t1)
+#             former_value = bag_rec_unique[removal_index_1][3]
+#             for index_2, bag_2 in enumerate(list(bag_rec_unique)):
+#                 if bag_t1 in bag_2[1]:
+#                     removal_index_2 = bag_rec_unique[index_2][1].index(bag_t1)
+#                     value = bag_rec_unique[index_2][2][removal_index_2]
+#                     bag_rec_unique[index_2][3] += value * former_value
+#                     del(bag_rec_unique[index_2][1][removal_index_2])
+#                     del(bag_rec_unique[index_2][2][removal_index_2])
+#             print(bag_rec_unique[removal_index_1], removal_index_1)
+#             del(bag_rec_unique[removal_index_1])
+
+
+bag_rec_unique_original = deepcopy(bag_rec_unique)
+bag_t1 = None
+exit_loop = False
+
+for iter_i in range(7):
+
+    # if exit_loop is True:
+    #     break
+
+    for bag_1 in list(bag_rec_unique):
+        
+        # removal_index_main = [i[0] for i in bag_rec_unique].index('shiny gold')
+        # print("{0}".format(bag_rec_unique[removal_index_main]))
+        # if len(bag_rec_unique[removal_index_main][1]) <= 1:
+        #     exit_loop =  True
+        #     break
+
+        if len(bag_1[1]) == 0:
+            bag_t1 = bag_1[0]
+            removal_index_1 = [i[0] for i in bag_rec_unique].index(bag_t1)
+            former_value = bag_rec_unique[removal_index_1][3]
+            for bag_2 in list(bag_rec_unique):
+                if bag_t1 in bag_2[1]:
+                    removal_index_2 = [i[0] for i in bag_rec_unique].index(bag_2[0])
+                    removal_index_3 = bag_2[1].index(bag_t1)
+                    value = bag_2[2][removal_index_3]
+                    bag_rec_unique[removal_index_2][3] += (value * former_value)
+                    del(bag_rec_unique[removal_index_2][1][removal_index_3])
+                    del(bag_rec_unique[removal_index_2][2][removal_index_3])
+            # print(bag_rec_unique[removal_index_1], removal_index_1)
+            del(bag_rec_unique[removal_index_1])
+    
+    # print(list(set([i[0] for i in bag_rec_unique_original]) - set([i[0] for i in bag_rec_unique])))
+
+    print("After Iteration\t{0}\n".format(iter_i))
+    for j in range(len(bag_rec_unique)):
+        print(bag_rec_unique[j],"\n")
+    print("\n\n")
+    
+
+
+# for bag in (bag_rec_unique):
+#     print("Main Bag : {0} \nSub Bags : {1}\nBag Count: {2}\nTotal Count: {3}\n".format(bag[0], bag[1], bag[2], bag[3]))
+
 
 # Excluding initial entry 'shiny gold' from the bag list.
 print("Total Bags 2 : {0} \n".format(len((bags_to_check_2[1:]))))
